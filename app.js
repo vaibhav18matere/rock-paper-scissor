a38734d546fdce85ad6a68da62d8ad3894705dbb
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-03-05 23:06:29 +0530

    fetch result statement

--- app.js
+++ app.js
@@ -1,53 +1,66 @@
-console.log("hello!");
+// console.log("hello!");
 
-//catching the DOM for future use
-const userScore=0;
-const computerScore=0;
+//1. catching the DOM for future use
+var userScore=0;
+var computerScore=0;
 
 const userScore_span =document.getElementById('user-score');
 const computerScore_span =document.getElementById('computer-score');
 
 scoreBoard_div = document.querySelector('.score-board');
-scoreBoard_div = document.querySelector('.result');
+result_p = document.querySelector('.result >p');
 
 const rock_div=document.getElementById('r');
 const paper_div=document.getElementById('p');
 const scissor_div=document.getElementById('s');
 
-//generate r,p,s randomly from computer
+//3. generate r,p,s randomly from computer
 function getComputerChoice(){
     const choices=['r','p','s']; 
     const randomNumber=Math.floor(Math.random()*3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice);
+//5. fetch result statement
+function win (userChoice,computerChoice){
+    userScore++;
+    userScore_span.innerHTML=userScore;
+    computerScore_span.innerHTML=computerScore;
+    result_p.innerHTML=userChoice+" beats "+computerChoice+" you win!";
+}
 
-//making a game function to feth both scores
+function lose (){
+    console.log("lose");
+}
+
+function draw (){
+    console.log("draw");
+}
+
+
+//4 . making a game function to feth both scores ;also condition for winning , or draw
 function game (userChoice){
     const computerChoice=getComputerChoice();
-
     switch (userChoice+computerChoice) {
 
         case "rs":
         case "pr":
         case "sp":
-            console.log("you win")  
+            win(userChoice,computerChoice);  
             break;
         case "sr":
         case "rp":
         case "ps":
-            console.log("you lost")
+            lose(userChoice,computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            console.log("Its a Draw !!!")
+            draw(userChoice,computerChoice);
             break;
     }
-
 }
 
-//adding click event listener
+//2. adding click event listener
 function main(){
     rock_div.addEventListener('click', function (){
     game ('r');
